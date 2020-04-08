db3b75bb4f183ea01df96f91f1915c1aa42e7bf6
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2020-04-08 20:34:26 +0800

    Update k_mean.py

--- k_mean.py
+++ k_mean.py
@@ -4,3 +4,15 @@ from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 
+k=4 #number of clusters
+n_samples=1500
+random_state=17
+
+X,y=make_blobs(n_samples=n_samples,centers=k,n_features=2,random_state=random_state)
+
+color='black'
+size=1
+
+plt.figure()
+plt.scatter(X[:,0],X[:,1],c=color,s=size)
+
